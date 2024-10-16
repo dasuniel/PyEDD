f1e909f841e575b225f2cdfb526b855c84a077f5
dasuniel <115125034+dasuniel@users.noreply.github.com>
2024-10-16 14:44:07 -0500

    Update Arista.h

--- Arista.h
+++ Arista.h
@@ -8,16 +8,23 @@
   
     public:
     unsigned int obtenerIndiceAr();
+
     std::list<Vertice> obtenerListaVertices();
   
     void fijarIndiceAr(unsigned int index);
+
     void fijarListaVertices(std::list<Vertice> lista);
+
     void fijarVertices(unsigned int x, unsigned int y, unsigned int z);
   
     void imprimirArista();
+
     void agregarVertice(Vertice vertex);
+
     void eliminarVertice(int vertexIndex);
+
     unsigned int contarVertices(std::list<Vertice> lista);
+
     Arista();
     
     };
