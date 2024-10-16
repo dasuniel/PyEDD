ce89f0d49afddc525fa5de4207aed0c98b08d487
dasuniel <115125034+dasuniel@users.noreply.github.com>
2024-10-16 15:41:34 -0500

    Update Objeto.h

--- Objeto.h
+++ Objeto.h
@@ -1,26 +1,35 @@
+#ifndef OBJETO_H
+#define OBJETO_H
+
 #include <string>
 #include <list>
 #include "Cara.h"
+#include "Vertice.h"  
 
 class Objeto {
-    private:
-        std::string nombreObjeto;
-        std::list<Cara> listaCaras;
-        unsigned int cantidadVerticesObj;
+private:
+    std::string nombreObjeto;
+    std::list<Cara> listaCaras;
+    unsigned int cantidadVerticesObj;
+    Vertice esquinas[8];  // Array para almacenar las ocho esquinas del objeto envolvente.
 
-    public:
-        std::string obtenerNombreObjeto(); 
-        std::list<Cara> obtenerCaras();
-        int obtenerCantidadVerticesObj();
+public:
+    Objeto();  // Constructor
+    std::string obtenerNombreObjeto();
+    std::list<Cara> obtenerCaras();
+    int obtenerCantidadVerticesObj();
 
-        void fijarNombreObjeto(std::string objectName);  
-        void fijarListaCaras(std::list<Cara> listaCaras);
-        void fijarCantidadVerticesObj(int cantVertices);
+    void fijarNombreObjeto(std::string objectName);
+    void fijarListaCaras(std::list<Cara> listaCaras);
+    void fijarCantidadVerticesObj(int cantVertices);
 
-        void agregarCara(Cara face);
-        void eliminarCara(int faceIndex);  
-        unsigned int contarCaras();
+    void agregarCara(Cara face);
+    void eliminarCara(int faceIndex);
+    unsigned int contarCaras();
 
-        void imprimirObjeto();
-        Objeto();
+    void imprimirObjeto();
+    std::vector<Vertice> obtenerEsquinas() ;
+    void imprimirEsquinas();  // Método para imprimir las esquinas
 };
+
+#endif // OBJETO_H
