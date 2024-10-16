0dc0aede6227ef5ded8d92b9846aecc1a276371f
dasuniel <115125034+dasuniel@users.noreply.github.com>
2024-10-16 14:49:09 -0500

    Update Cara.h

--- Cara.h
+++ Cara.h
@@ -4,21 +4,31 @@
     class Cara{
         private:
         unsigned int indiceCar;
+
         unsigned int cantidadVerticesCar;
+
         std::list<Arista> listaAristas;
     
         public:
         unsigned int obtenerIndiceCar();
+
         unsigned int obtenerCantidadVerticesCar();
+
         std::list<Arista> obtenerListaAristas();
 
         void fijarIndiceCar(unsigned int indice);
+
         void fijarCantidadVerticesCar(unsigned int cantVertices);
+
         void fijarListaAristas(std::list<Arista> lista);
 
         void imprimirCara();
+
         void agregarArista(Arista edge);
+
         void eliminarArista(int edgeIndex);
+
         unsigned int contarAristas(std::list<Arista> lista);
+
         Cara();
     };
