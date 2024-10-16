a97fd4d8e23dc115b754bba331f2e564b93e4803
dasuniel <115125034+dasuniel@users.noreply.github.com>
2024-10-16 16:25:02 -0500

    Update Vertice.h

--- Vertice.h
+++ Vertice.h
@@ -1,24 +1,37 @@
+#ifndef VERTICE_H
+#define VERTICE_H
 
-    class Vertice{
-        private:
-        unsigned int indiceVer;
-        unsigned int x;
-        unsigned int y;
-        unsigned int z;
-    
-        public:
-        unsigned int obtenerX();
-        unsigned int obtenerY();
-        unsigned int obtenerZ();
-        unsigned int obtenerIndiceVer();
+#include <vector>
 
-        void fijarX(unsigned int x);
-        void fijarY(unsigned int y);
-        void fijarZ(unsigned int z);
-        void fijarIndiceVer(unsigned int index);
+class Vertice {
+private:
+    unsigned int indiceVer;
+    unsigned int x;
+    unsigned int y;
+    unsigned int z;
 
-        void imprimirVertice();
-        Vertice();
-    };
+public:
+    // Constructor
+    Vertice();
+    Vertice(unsigned int x, unsigned int y, unsigned int z);
+
+    // Getters
+    unsigned int obtenerX();
+    unsigned int obtenerY();
+    unsigned int obtenerZ();
+    unsigned int obtenerIndiceVer();
     
-    
+    // Setters
+    void fijarX(unsigned int x);
+    void fijarY(unsigned int y);
+    void fijarZ(unsigned int z);
+    void fijarIndiceVer(unsigned int index);
+
+    // Método para imprimir el vértice
+    void imprimirVertice();
+
+    // Método para calcular las esquinas de la caja envolvente
+    std::vector<Vertice> calcularEsquinasCaja(unsigned int xmin, unsigned int xmax, unsigned int ymin, unsigned int ymax, unsigned int zmin, unsigned int zmax);
+};
+
+#endif // VERTICE_H
